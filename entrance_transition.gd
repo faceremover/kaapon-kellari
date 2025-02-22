0b48aa1fd9c77a8d83cbc7bec246e308ab675080
faceremover <83660328+faceremover@users.noreply.github.com>
2025-02-22 20:24:39 +0200

    flashier intro + new map prototype

--- entrance_transition.gd
+++ entrance_transition.gd
@@ -6,27 +6,27 @@ extends Control
 @onready var exit_button = $ExitButton
 
 func _ready():
-    start_button.connect("pressed", self, "_on_start_button_pressed")
-    exit_button.connect("pressed", self, "_on_exit_button_pressed")
-    enter_video.connect("finished", self, "_on_enter_video_finished")
-    exit_video.connect("finished", self, "_on_exit_video_finished")
+	start_button.connect("pressed", self, "_on_start_button_pressed")
+	exit_button.connect("pressed", self, "_on_exit_button_pressed")
+	enter_video.connect("finished", self, "_on_enter_video_finished")
+	exit_video.connect("finished", self, "_on_exit_video_finished")
 
 func _on_start_button_pressed():
-    start_button.disabled = true
-    exit_button.disabled = true
-    enter_video.visible = true
-    enter_video.play()
+	start_button.disabled = true
+	exit_button.disabled = true
+	enter_video.visible = true
+	enter_video.play()
 
 func _on_exit_button_pressed():
-    start_button.disabled = true
-    exit_button.disabled = true
-    exit_video.visible = true
-    exit_video.play()
+	start_button.disabled = true
+	exit_button.disabled = true
+	exit_video.visible = true
+	exit_video.play()
 
 func _on_enter_video_finished():
-    enter_video.visible = false
-    hide()
-    get_tree().paused = false
+	enter_video.visible = false
+	hide()
+	get_tree().paused = false
 
 func _on_exit_video_finished():
-    get_tree().quit()
+	get_tree().quit()
